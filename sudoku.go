b594a4481233a0030856579e639cd364632c08bb
u50147 <u50147@d4dbdaaf-26a9-43fc-8ea5-ed94ec5ec015.prvt.dyno.rt.heroku.com>
2016-03-25 10:17:53 +0000

    Initial commit

---
+++ sudoku.go
@@ -0,0 +1,47 @@
+﻿package sudoku
+
+func Solve(data [][]int) [][]int {
+	return data
+}
+
+
+func CheckRow(data [][]int, x int, y int, val int) bool {
+	for ix := 0; ix < 9; ix++ {
+		if ix == x { 
+			continue 
+		}
+		if data[ix][y] == val { 
+			return false 
+		}
+	}
+	return true
+}
+
+func CheckColumn(data [][]int, x int, y int, val int) bool {
+	for iy := 0; iy < 9; iy++ {
+		if iy == y { 
+			continue 
+		}
+		if data[x][iy] == val {
+			return false 
+		}
+	}
+	return true
+}
+
+func CheckCell(data [][]int, x int, y int, val int) bool {
+	sx := x - (x % 3)
+	sy := y - (y % 3)
+
+	for ix := sx; ix < sx + 3; ix++ {
+		for iy := sy; iy < sy + 3; iy++ {
+			if y == iy && x == ix { 
+				continue 
+			}
+			if data[ix][iy] == val { 
+				return false 
+			}
+		}
+	}
+	return true
+}
